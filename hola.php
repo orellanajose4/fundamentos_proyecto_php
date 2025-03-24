62d11e5a7ac8d0db38c92bfcd713528786dd3468
orellanajose4 <orellanajose4@hotmail.com>
2025-03-23 21:36:11 -0600

    initial commit

---
+++ hola.php
@@ -0,0 +1,12 @@
+<?php
+
+$variable_nombre = "Jose Orellana Flores";
+
+echo "Hola mundo, soy " . $variable_nombre;
+
+?>
+
+<img src="/Actividad3/proyecto_php/pitbull.jpeg" alt="Descripcion de la imagen">
+
+
+
